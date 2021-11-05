652b0772aa08c081f9306f83d34562de952e728c
MoriczJanos113C <Sh2rm56kL>
2021-11-05 08:07:38 +0100

    database version

---
+++ src/main/java/hu/clubsWebService/domain/Club.java
@@ -0,0 +1,71 @@
+package hu.clubsWebService.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Club {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String address;
+    private int guests;
+
+    @ManyToOne
+    private Category category;
+
+    public Club() {
+    }
+
+    @Override
+    public String toString() {
+        return "Club{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", space=" + guests +
+                ", category=" + category.getName() +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getGuests() {
+        return guests;
+    }
+
+    public void setGuests(int guests) {
+        this.guests = guests;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
